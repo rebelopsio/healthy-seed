2bcd6993abb6144286aed9817b7aa92f013650f7
Stephen Morgan <steve@rebelops.io>
2022-01-30 12:15:09 -0800

    created webserver, flag for config file, handling of config file
    Signed-off-by: Stephen Morgan <steve@rebelops.io>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Function to handle errors. From Go by Example
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// Version place holder
+var Version = "0.0.1"
+
+// Create a struct for config file
+type envConfig struct {
+	DSN           string `yaml:"dsn"`
+	JwtSigningKey string `yaml:"jwt_signing_key"`
+}
+
+// Create a var for the struct
+var configParams envConfig
 
 func main() {
-	fmt.Println("This is the start of healthy-seed")
+	// Config file flag
+	var configPtr string
+	flag.StringVar(&configPtr, "config", "local.yml", "config file path")
+	flag.Parse()
+	dat, err := os.ReadFile(configPtr)
+	check(err)
+
+	err = yaml.Unmarshal(dat, &configParams)
+	check(err)
+
+	// fmt.Println(&configParams.DSN, &configParams.JwtSigningKey)
+	http.HandleFunc("/", handler) // Each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\nDSN = %q\nJWT = %q\n", r.URL.Path, configParams.DSN, configParams.JwtSigningKey)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rebelopsio/healthy-seed
 
 go 1.17
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
