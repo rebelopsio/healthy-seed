9d5c39d3b6c8687a0da8d5a1112fd0a8b74699a4
Stephen Morgan <steve@rebelops.io>
2022-03-05 09:48:37 -0800

    added additional CRUD operations for accounts, updated Makefile help info
    Signed-off-by: Stephen Morgan <steve@rebelops.io>

--- db/query/account.sql
+++ db/query/account.sql
@@ -10,3 +10,27 @@ INSERT INTO accounts (
 ) VALUES (
 	$1, $2, $3, $4, $5, $6, $7
 ) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccountWeight :exec
+UPDATE accounts
+SET current_weight = $2
+WHERE id = $1;
+
+-- name: UpdateAccountAge :exec
+UPDATE accounts
+SET age = $2
+WHERE id = $1;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
